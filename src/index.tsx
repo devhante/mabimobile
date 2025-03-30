15cd7079b160397b4b02de618150196de0c9df95
Yujun Cho <devhante@gmail.com>
2025-03-31 04:41:34 +0900

    basename 추가

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
       </QueryClientProvider>
